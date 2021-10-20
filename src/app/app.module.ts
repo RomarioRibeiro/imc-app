51e77f8ef1731d1359ecd269499cb688a696b634
ROMARIORIBEIRO <romaryjane@hotmail.com>
2021-10-19 21:39:19 -0300

    correções

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { PrimengModule } from './primeng.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     PrimengModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
